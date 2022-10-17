6efc0f7e3b504e0fe67e57c6a50524128b4406eb
deegyi <dee_gyi@live.ca>
2022-10-17 14:47:30 -0400

    ok

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('tasks/', views.tasks_index, name='index'),
-    path('/', views.TaskCreate.as_view(), name='tasks_create'),
+    path('tasks/create/', views.TaskCreate.as_view(), name='tasks_create'),
     path('tasks/<int:pk>/delete/', views.TaskDelete.as_view(), name='tasks_delete'),
 ]
